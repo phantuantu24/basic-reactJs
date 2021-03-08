0174bc718d24f1d900c51919c6443bbf7050bf07
TuPT <nhokkon2497@gmail.com>
2021-03-08 16:30:58 +0700

    re-write fetching data basically

---
+++ src/components/BasicFetchingData.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios'
+
+function BasicFetchingData() {
+
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState('')
+  const [post, setPost] = useState({})
+
+  useEffect(() => {
+    Axios.get('https://jsonplaceholder.typicode.com/post/1')
+      .then(res => {
+        setLoading(false)
+        setError('')
+        setPost(res.data)
+      })
+      .catch(err => {
+        setLoading(false)
+        setPost({})
+        setError(`Something went wrong - ${err}`)
+      })
+  }, [])
+
+  return (
+    <div>
+      { loading ?  "Loading..." : post.title }
+      { error ? error : null}
+    </div>
+  )
+}
+
+export default BasicFetchingData
