f621de9ae2c86322008837fbb32050ee62a2f2da
TuPT <nhokkon2497@gmail.com>
2021-03-08 14:38:52 +0700

    simple state and action with useReducer

---
+++ src/component.js/UseReducerCounter.js
@@ -0,0 +1,31 @@
+import React, { useReducer } from 'react'
+
+const initialCount = 0
+const countReducer = (state, action) => {
+  switch (action) {
+    case 'increment':
+      return state + 1
+    case 'decrement':
+      return state - 1
+    case 'reset':
+      return initialCount
+    default:
+      return state
+  }
+}
+
+function UseReducerCounter() {
+
+  const [count, dispatch] = useReducer(countReducer, initialCount)
+  
+  return (
+    <div>
+      <h3>Count - {count}</h3>
+      <button onClick={() => dispatch('increment')}>Increment</button>
+      <button onClick={() => dispatch('decrement')}>Decrement</button>
+      <button onClick={() => dispatch('reset')}>Reset</button>
+    </div>
+  )
+}
+
+export default UseReducerCounter
